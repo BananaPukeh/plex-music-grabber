e5df745ce72aeae1faee926bcf39d2357134b2a0
Rutger Nijhuis <rutger@nyhuis.dev>
2022-03-15 22:24:16 +0100

    Updated automated tagging of images

--- update-image.sh
+++ update-image.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 IMAGE=rutgernijhuis/plex-music-grabber
 
-docker build -t $IMAGE .
-docker push $IMAGE
+TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
+
+docker build -t $IMAGE:$TAG .
+docker push $IMAGE:$TAG
