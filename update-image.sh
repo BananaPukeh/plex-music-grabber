4d1d60f51de8dec8e2feafd3d12dbc9c7f6e1b80
Rutger Nijhuis <rutger.nijhuis@gmail.com>
2022-07-18 19:44:13 +0200

    switched to alpine

--- update-image.sh
+++ update-image.sh
@@ -3,7 +3,7 @@ IMAGE=rutgernijhuis/plex-music-grabber
 
 TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
 
-docker build -t $IMAGE .
+docker build --no-cache -t $IMAGE .
 docker push $IMAGE
 
 docker tag $IMAGE $IMAGE:$TAG
